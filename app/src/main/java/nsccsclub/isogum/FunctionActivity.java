7fd028d138a64138614c5af79bd05f1e54610652
collin <collinsconway@gmail.com>
2016-03-11 17:32:27 -0800

    1st commit for tutorial

--- app/src/main/java/nsccsclub/isogum/FunctionActivity.java
+++ app/src/main/java/nsccsclub/isogum/FunctionActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 
 public class FunctionActivity extends AppCompatActivity {
 
+    //welcome nsc comp sci club
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/nsccsclub/isogum/delete.java
@@ -0,0 +1,7 @@
+package nsccsclub.isogum;
+
+/**
+ * Created by csconway on 3/11/2016.
+ */
+public class delete {
+}
