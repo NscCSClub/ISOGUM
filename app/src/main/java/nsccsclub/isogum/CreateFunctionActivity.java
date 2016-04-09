36f8bf4aa5a2d787d96793e0f1e856f7d272376e
collin <collinsconway@gmail.com>
2016-04-09 12:49:22 -0700

    Added screeen for variable and funciton and buttons enables scrolling for easy input

--- app/src/main/java/nsccsclub/isogum/CreateFunctionActivity.java
+++ app/src/main/java/nsccsclub/isogum/CreateFunctionActivity.java
@@ -3,6 +3,7 @@ package nsccsclub.isogum;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * The creation screen for an uncertainty object, currently being implemented
@@ -17,8 +18,12 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class CreateFunctionActivity extends AppCompatActivity {
 
+    //class constants used for storing and retrieving temporary data
+    static final String TEMP_NAME = "function_name";
+    static final String TEMP_FUNCTION = "function_function";
 
     private InputKeyboard inputKeyboard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,29 @@ public class CreateFunctionActivity extends AppCompatActivity {
         inputKeyboard = new InputKeyboard(this,R.id.keyboardview_function,R.layout.keyboard_layout);
         inputKeyboard.registerEditText(R.id.create_function_name_bar);
         inputKeyboard.registerEditText(R.id.create_function_value_bar);
+
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore state members from saved instance
+        ((EditText)findViewById(R.id.create_function_name_bar)).setText(
+                savedInstanceState.getString(TEMP_NAME));
+        ((EditText)findViewById(R.id.create_function_value_bar)).setText(
+                savedInstanceState.getString(TEMP_FUNCTION));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the current state of all text fields
+        savedInstanceState.putString(TEMP_NAME,
+                ((EditText)findViewById(R.id.create_function_name_bar)).getText().toString());
+        savedInstanceState.putString(TEMP_FUNCTION,
+                ((EditText) findViewById(R.id.create_function_value_bar)).getText().toString());
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
