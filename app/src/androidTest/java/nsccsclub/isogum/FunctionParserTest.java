189d9930cdeb746c9be5cd288c6b4978e15227ac
collin <collinsconway@gmail.com>
2016-05-01 17:13:05 -0700

    added function parser

---
+++ app/src/androidTest/java/nsccsclub/isogum/FunctionParserTest.java
@@ -0,0 +1,26 @@
+package nsccsclub.isogum;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by csconway on 5/1/2016.
+ */
+public class FunctionParserTest extends TestCase {
+
+
+    public void testGetNext() throws Exception {
+
+    }
+
+    public void testGetPrev() throws Exception {
+
+    }
+
+    public void testHasNext() throws Exception {
+
+    }
+
+    public void testSetFunction() throws Exception {
+
+    }
+}
